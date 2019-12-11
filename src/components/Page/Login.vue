aaba27dd62a73f356a5d6af84110e4a937965310
Ubuntu <ubuntu@ip-172-31-19-54.ap-northeast-2.compute.internal>
2019-12-11 12:49:05 +0000

    20191211 naver로그인 추가수정 운영반영

--- src/components/Page/Login.vue
+++ src/components/Page/Login.vue
@@ -86,13 +86,14 @@
       //this.callbackUrl = 'http://52.79.240.238:8080/callback'
       //운영
       this.client_id = 'Vwg5qvZi4T3pagL1ZASv'
-      this.callbackUrl = 'http://hereluxury.com/callback'
+      //this.callbackUrl = 'http://hereluxury.com/callback'
+      this.callbackUrl = 'http://54.180.120.131:4000/callback'
 
     },
     methods:{
       naverlogin(){
         var url = 'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id='+this.client_id+'&redirect_uri='+this.callbackUrl+'&state=1234';
-        window.location.href = url;
+        this.$route.router.go('/');
       },
       onSubmit() {
         if(this.email_flag){
