404d275d4bd9557ec29e23b27c5c87c69ed066f1
Ubuntu <ubuntu@ip-172-31-19-54.ap-northeast-2.compute.internal>
2020-01-15 14:45:36 +0000

    20200115 검색기능추가 운영반영

--- src/components/Card/SearchProductCard.vue
+++ src/components/Card/SearchProductCard.vue
@@ -7,7 +7,7 @@
                 <img class="lazy-img-fadein"  v-lazy="item.url" style="width: 130px; height: 130px;"alt="..." >
                   <div class="lazy-img-fadein" v-lazy:background-image="item.url"></div>
                   <h6> <br /> <b>{{item.name}}</b> </h6>
-                  <h6> <br /> <b>{{item.price}}</b> </h6>
+                  <h6> <b>{{item.price}}</b> </h6>
               </div>
             </div>
           </div>
