e56a5a8c6f0654cae8d28a5eb5f24d066d5ce6fc
Ubuntu <ubuntu@ip-172-31-19-54.ap-northeast-2.compute.internal>
2020-01-27 14:30:16 +0000

    20200127 일부오류 수정 운영반영

--- src/components/Page/Search.vue
+++ src/components/Page/Search.vue
@@ -144,7 +144,7 @@ export default{
         for(var j=0; j<this.code.length; j++){
           var descript = this.code[j].descript;
           descript = descript.replace(/ /gi, "");
-          if(this.code[j].descript.indexOf(word) != -1){
+          if(descript.indexOf(word) != -1){
             var codeObj = new Object();
             if(this.code[j].major_key == 1 || this.code[j].major_key == 7778){
               codeObj.code = 'brand_name';
