a5d9e3ae8902f41fb88e673b1bbc279cd5087815
Ubuntu <ubuntu@ip-172-31-19-54.ap-northeast-2.compute.internal>
2020-02-17 14:04:27 +0000

    20200217 애드센스 광광고정테스트  운영반영

--- src/components/Page/Test.vue
+++ src/components/Page/Test.vue
@@ -1,17 +1,17 @@
 <template>
   <div style="padding-top:100px;">
-    <div id="adsgoeshere" style="background: #1d1f29; padding-top:60px; text-align: center;" v-html="adsenseContent"></div>
+    <ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-4141026730478876"
+     data-ad-slot="6033471797"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
   </div>
 </template>
 <script>
 export default{
-  data(){
-    return{
-      adsenseContent: ''
-    }
-  },
   mounted(){
-    this.adsenseContent = document.getElementById('divadsensedisplaynone').innerHTML
+    (adsbygoogle = window.adsbygoogle || []).push({});
   }
 
 }
