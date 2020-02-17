3d2f42b040b13464d5fd4b16fc99904e7b98cfb2
Ubuntu <ubuntu@ip-172-31-19-54.ap-northeast-2.compute.internal>
2020-02-17 13:15:03 +0000

    20200217 애드센스 광광고정테스트  운영반영

---
+++ src/components/Page/Test.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <ins class="adsbygoogle"
+     style="display:block"
+     data-ad-format="fluid"
+     data-ad-layout-key="-fb+5w+4e-db+86"
+     data-ad-client="ca-pub-4141026730478876"
+     data-ad-slot="8270659843"></ins>
+  </div>
+</template>
+<script>
+export default{
+  mounted(){
+    (adsbygoogle = window.adsbygoogle || []).push({});
+  }
+
+}
+</script>
