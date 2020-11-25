7833c3f909bec1acc3f64cfc91efa56fe2391e06
Ubuntu <ubuntu@ip-172-31-19-54.ap-northeast-2.compute.internal>
2020-11-25 14:02:42 +0000

    20201125 로그인후 상품상세로 이동

--- src/components/Page/Callback.vue
+++ src/components/Page/Callback.vue
@@ -137,7 +137,7 @@
             localStorage.setItem('id', this.loginArry.response.email)
             localStorage.setItem('type', 'naver')
             setAuthInHeader(this.authArry.refresh_token) //token
-            this.$router.push(this.$route.query.redirect);
+            this.$router.push(this.loginreturnPath);
           } else {
 
           }
